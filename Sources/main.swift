58fd641e75ba6e15c1ab933a5f284c69d4cc9cb2
Felix Nievelstein <felix.nievelstein@appside.me>
2018-12-09 21:53:01 +0100

    Fixing issue

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,27 +1,3 @@
-import Glibc
 import Foundation
-import SwiftyGPIO
-
-func setGPIO(){
-	let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPiPlusZero)
-	guard let ledGPIO = gpios[.P7] else {
-        	fatalError("It has not been possible to initialised the LED GPIO pin")
-	}
-	print("Setting GPIOs")
-	switchLED(ledGPIO: ledGPIO)
-}
-
-
-func switchLED(ledGPIO: GPIO){
-	
-	ledGPIO.direction = .OUT
-	while (true) {
-		print("Switching signal")
-		sleep(1)
-		ledGPIO.value = 1
-		sleep(0)
-		ledGPIO.value = 0
-	}
-}
 
 setGPIO();
