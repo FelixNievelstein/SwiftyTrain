4422f34eeb0fd763870df3bbce0ddcfc9d97d639
Felix Nievelstein <felix.nievelstein@appside.me>
2018-12-09 20:50:12 +0100

    Adding blinking led

--- Sources/ledTest2.swift
+++ Sources/ledTest2.swift
@@ -8,6 +8,18 @@ func setGPIO(){
 	guard let ledGPIO = gpios[.P7] else {
         	fatalError("It has not been possible to initialised the LED GPIO pin")
 	}
+	switchLED(ledGPIO: ledGPIO)
+}
+
+
+func switchLED(ledGPIO: GPIO){
+	ledGPIO.direction = .OUT
+	while (true) {
+		sleep(1)
+		ledGPIO.value = 1
+		sleep(0)
+		ledGPIO.value = 0
+	}
 }
 
 setGPIO()
