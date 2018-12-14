3ae147cfe686160adafac549a5223db43336f2eb
Felix Nievelstein <felix.nievelstein@appside.me>
2018-12-15 00:25:09 +0100

    fixing package import

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "ledTest2",
 	 dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 1)
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/BluetoothLinux.git", majorVersion: 3)
     ]
 )
