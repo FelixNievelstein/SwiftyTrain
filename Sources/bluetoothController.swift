c249c0c0350b0e5e16350eeb7ed27e0f9b116bff
Felix Nievelstein <felix.nievelstein@appside.me>
2018-12-29 20:55:26 +0100

    adding advertising parameters

--- Sources/bluetoothController.swift
+++ Sources/bluetoothController.swift
@@ -8,7 +8,18 @@ class STBluetoothController {
             return;
         }
         do{
+            let parameters = HCILESetAdvertisingParameters.init(interval: (min: AdvertisingInterval.init(rawValue: 100)!, max: AdvertisingInterval.init(rawValue: 5000)!), 
+                                advertisingType: .directed, 
+                                ownAddressType: .public, 
+                                directAddresssType: .public, 
+                                directAddress: .zero,
+                                channelMap: .all,
+                                filterPolicy: .any)             
+
+            try hostController.setLowEnergyAdvertisingParameters(parameters,
+                                       timeout: .default)
             try hostController.enableLowEnergyAdvertising(true, timeout: .default)
+
             //hostController.setPeriodicAdvertisingEnable(enable: HCILESetPeriodicAdvertisingEnable.Enable, advertisingHandle: UInt8, timeout: .default)
         }
         catch {
